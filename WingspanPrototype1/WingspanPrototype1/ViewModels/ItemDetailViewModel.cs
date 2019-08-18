4dbe2b0bfb968b37248cd396a07ea8961c6f2bb6
stefan keogan <stefan.331@hotmail.com>
2019-08-19 11:11:13 +1200

    Initial Commit

---
+++ WingspanPrototype1/WingspanPrototype1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WingspanPrototype1.Models;
+
+namespace WingspanPrototype1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
