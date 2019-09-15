cc31d3113fc7ae19aae05276d2bf469326671620
James Cooper <mrjbcooper@gmail.com>
2019-09-16 10:39:37 +1200

    Added sponsorship page

---
+++ WingspanPrototype1/WingspanPrototype1/View/AddSponsorship.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WingspanPrototype1.View
+{
+    public partial class AddSponsorship : ContentPage
+    {
+        public AddSponsorship()
+        {
+            InitializeComponent();
+        }
+    }
+}
