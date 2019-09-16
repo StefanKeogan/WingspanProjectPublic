c9a4733f1d2986190260befc77368f8478a54399
James Cooper <mrjbcooper@gmail.com>
2019-09-17 10:29:35 +1200

    New classes

---
+++ WingspanPrototype1/WingspanPrototype1/Model/Sponsor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WingspanPrototype1.Model
+{
+    public class Sponsor
+    {
+        public int SponsorID { get; set; }
+        public string SponsorName { get; set; }
+        public string SponsorAddress { get; set; }
+        public string SponsorPhone { get; set; }
+        public string SponsorEmail { get; set; }
+    }
+}
---
+++ WingspanPrototype1/WingspanPrototype1/Model/Sponsorship.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WingspanPrototype1.Model
+{
+    public class Sponsorship
+    {
+        public int SponsorshipID { get; set; }
+        public string SponsorshipCategory { get; set; }
+        public DateTime SponsorshipStart { get; set; }
+        public DateTime SponsorshipEnd { get; set; }
+    }
+}
