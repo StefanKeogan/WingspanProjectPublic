7577735e18cefca06764f0f9bf05a9580251afc4
James Cooper <mrjbcooper@gmail.com>
2019-09-16 13:54:49 +1200

    Work done on Sponsors

--- WingspanPrototype1/WingspanPrototype1/View/AddSponsorship.xaml.cs
+++ WingspanPrototype1/WingspanPrototype1/View/AddSponsorship.xaml.cs
@@ -1,15 +1,46 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace WingspanPrototype1.View
 {
     public partial class AddSponsorship : ContentPage
     {
-        public AddSponsorship()
+        public AddSponsorship(string title)
         {
             InitializeComponent();
+            Title = title; //gives the detail page the title passed from the main page
+        }
+
+        //sorts out 
+        async void OnBirdSelected (object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex == 0)
+            {
+                await Navigation.PushAsync()
+            }
+            else if (selectedIndex == 1)
+            {
+
+            }
+        }
+
+
+        async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            bool answer = await DisplayAlert("Confirmation", "Would you like to save this sponsorship?", "Yes", "No");
+
+            if (answer == true)
+            {
+                //need to connect these things to the database as well
+                await DisplayAlert("Saved", "This sponsorship is now active", "Ok");
+            }
+            //DisplayAlert("Sponsorship Confirmed", "This bird is now being sponsored", "Ok");
         }
     }
 }
