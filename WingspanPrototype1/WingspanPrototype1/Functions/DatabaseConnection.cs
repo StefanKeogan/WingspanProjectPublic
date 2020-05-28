2077955e0cbe68f913bc7b1bb48ef953d362895e
stefan keogan <stefan.331@hotmail.com>
2020-05-28 17:04:08 +1200

    Stripped out wingspans db creds

--- WingspanPrototype1/WingspanPrototype1/Functions/DatabaseConnection.cs
+++ WingspanPrototype1/WingspanPrototype1/Functions/DatabaseConnection.cs
@@ -12,14 +12,8 @@ namespace WingspanPrototype1
         // Local host connection string 
         // protected string connectionString = "mongodb://localhost:27017";
 
-        // Dedicated Scale grid test connections string
-        // protected string connectionString = "mongodb://birdbook:BirdBookIsAwesome19@SG-DedicatedTest-27835.servers.mongodirector.com:27017/WingspanDB";
-
-        // Wingspan Database connection string 
-        protected string connectionString = "mongodb://birdbook:BirdBookIsAwesome19@SG-WingspanDB-27577.servers.mongodirector.com:50454,SG-WingspanDB-27578.servers.mongodirector.com:50454,SG-WingspanDB-27579.servers.mongodirector.com:50454/WingspanDB?replicaSet=RS-WingspanDB-0&ssl=true";
-
         // Clever cloud connection 
-        // protected string connectionString = "mongodb://uf5r4cvqwjrfnced0id7:KyixXt51y8XZVmLyd8n6@boeuzmf4hwi4iiw-mongodb.services.clever-cloud.com:27017/boeuzmf4hwi4iiw";
+        protected string connectionString = "mongodb://uf5r4cvqwjrfnced0id7:KyixXt51y8XZVmLyd8n6@boeuzmf4hwi4iiw-mongodb.services.clever-cloud.com:27017/boeuzmf4hwi4iiw";
 
         protected MongoClient client;
         protected IMongoDatabase database;
@@ -32,18 +26,18 @@ namespace WingspanPrototype1
                 var clientSettings = MongoClientSettings.FromConnectionString(connectionString);
 
                 // SSL Settings only needed for Wingspan Database connection 
-                clientSettings.SslSettings = new SslSettings();
-                clientSettings.SslSettings.CheckCertificateRevocation = false;
-                clientSettings.UseTls = true;
-                clientSettings.AllowInsecureTls = true;
+                //clientSettings.SslSettings = new SslSettings();
+                //clientSettings.SslSettings.CheckCertificateRevocation = false;
+                //clientSettings.UseTls = true;
+                //clientSettings.AllowInsecureTls = true;
 
                 client = new MongoClient(clientSettings);
 
                 // Used for all connections other than clever cloud 
-                database = client.GetDatabase("WingspanDB");
+                // database = client.GetDatabase("WingspanDB");
                 
                 // Used for clever cloud connection
-                // database = client.GetDatabase("boeuzmf4hwi4iiw");
+                database = client.GetDatabase("boeuzmf4hwi4iiw");
 
                 return database;
             }
